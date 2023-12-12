37360b6ac95a868db7d81547dcb9f38cb1d54d17
Sofía Cauich <150540467+SofiaUTM@users.noreply.github.com>
2023-12-12 00:57:04 -0600

    Versión Final
    Ya terminé de agregar lo que quería del Java Script. Hay varios elementos demás, pero estoy satisfecha con el resultado :'3

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const nombre = "Sofía Saraí Cauich Vargas"
 
-alert(nombre);
+//alert(nombre);
 
 console.log(nombre);
 
 document.write(nombre);
+
+console.log(document.getElementById('título'));
+let elementoHTMLtítulo = document.getElementById('título');
+
+let elementoDeTypewriter = new Typewriter(elementoHTMLtítulo, {
+    loop: true
+});
+
+elementoDeTypewriter.typeString('¡Hola!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Soy Sofía Cauich')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Desarrolladora Web JR.')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Y Diseñadora Gráfica JR.')
+    .pauseFor(2500)
+    .start();
